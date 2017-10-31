4edba534e6b2c13050bbebed87802e6d9c951560
Katy <ihatetoast@gmail.com>
2017-10-31 07:55:48 -0500

    pseudocode added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(e) { 
-  console.log("if you see me, you dun did load");
+  console.log("if you see me, you dun did load gud.");
 });
+
+// PSEUDOCODE
+// need a list of words
+// need to grab one of those words at random
+// 	and need to store that word.
+// need to have an alphabet array
+
+// need to make spaces/dashes for that word
+
+// need to get user's guess for a letter
+// need to check user's guess against the randomword
+// 	if it's correct, letter appears in place of the dash
+// 	if it is incorrect, a body part is shown (so many jokes here) AND letter guessed is put into an incorrect box
+
+// ?store guessed letters in another array and remove them from alpha?
+// store incorrect guesses in an array. number of letters = number of parts shown?
+
+// announce win or loss
+// offer to play again
