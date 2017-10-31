7dbe845507b303179522aad269b3ad10b922b728
Katy <ihatetoast@gmail.com>
2017-10-31 07:44:31 -0500

    testing js ondomload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Hangman Game (Vanilla JS)
+
+## tools
+* Vanilla JavaScript
+* Sass
+* SVGs by me, so do not judge!
---
+++ assets/javascript/script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function(e) { 
+  console.log("if you see me, you dun did load");
+});
