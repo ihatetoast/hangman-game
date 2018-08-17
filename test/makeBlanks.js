fa88b231e06cd870f956989cda8eb3ad9ff4e95a
Katy <ihatetoast@gmail.com>
2018-08-17 12:43:31 -0500

    added test for dashes spaces apostrophes. added fcn to script js

--- test/makeBlanks.js
+++ test/makeBlanks.js
@@ -1,8 +1,23 @@
 var expect = require('chai').expect;
 var makeBlanks = require('../units/makeBlanks.js');
 
-describe('dude', function() {
-  it('it should show up in tests', function() {
-    expect(makeBlanks('')).to.equal('');
+describe('makeBlanks', function() {
+  it('it should replace letters with underscores', function() {
+    expect(makeBlanks('normal')).to.equal('_ _ _ _ _ _');
+  });
+  it('it should replace letters with underscores ignore hyphens', function() {
+    expect(makeBlanks('hyphen-like-this')).to.equal(
+      '_ _ _ _ _ _ - _ _ _ _ - _ _ _ _'
+    );
+  });
+  it('it should replace letters with underscores ignore spaces', function() {
+    expect(makeBlanks('phrase with spaces')).to.equal(
+      '_ _ _ _ _ _   _ _ _ _   _ _ _ _ _ _'
+    );
+  });
+  it('it should replace letters with underscores ignore apostrophes', function() {
+    expect(makeBlanks("momma's little helper")).to.equal(
+      "_ _ _ _ _ ' _   _ _ _ _ _ _   _ _ _ _ _ _"
+    );
   });
 });
